8d59cce7b9a2b8fa529512aa10d6eaafb782b100
Hex052 <elijahiff@gmail.com>
2019-04-27 17:52:25 -0800

    Implemented until comes to change icon

--- gui/actions/ChangeIcon.java
+++ gui/actions/ChangeIcon.java
@@ -1,26 +1,55 @@
 package gui.actions;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class ChangeIcon implements ActionListener {
 	public final input.Player usr;
+	private final JComboBox<File> selector = new JComboBox<>();
+	private final JFrame window = new JFrame();
+	private final JLabel message;
+	private final JButton select = new JButton("Select Image");
 	public static final ArrayList<File> fileNames = new ArrayList<>();
 	public ChangeIcon(input.Player player) {
 		usr = player;
+		message = new JLabel("Choose the icon for " + usr.name);
 		if (fileNames.size() == 0) {
 			genFiles(new File("img/piece"));
 		}
+		window.getContentPane().setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+		window.add(message);
+		window.add(selector);
+		window.add(select);
+		window.setTitle("Select icon");
+		window.setResizable(false);
+		select.addActionListener(this);
+		selector.setMaximumSize(new Dimension(600, 50));
 	}
 	public void actionPerformed(ActionEvent e) {
-
-
+		if (window.isAncestorOf((JButton)e.getSource())) {
+			changeTo((File)selector.getSelectedItem());
+			window.setVisible(false);
+			return;
+		}
+		selector.removeAllItems();
+		for (File f : fileNames) {
+			selector.addItem(f);
+		}
+		window.setSize(600, 150);
+		window.setVisible(true);
+	}
+	public void changeTo(File img) {
+		System.out.println(img);
+		//TODO Make this change icons
 		usr.icon.reloadIcon();
 	}
 	public static void genFiles(File parentDir) {
@@ -31,7 +60,7 @@ public class ChangeIcon implements ActionListener {
 		}
 		for (File path : parentDir.listFiles()) {
 			String fname = path.getName();
-			if (fname.isDirectory() || !fname.substring(fname.lastIndexOf('.') + 1).equals("png")) {
+			if (path.isDirectory() || !fname.substring(fname.lastIndexOf('.') + 1).equals("png")) {
 				continue;
 			}
 			fileNames.add(path);
